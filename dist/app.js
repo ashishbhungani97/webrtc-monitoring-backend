383aa3cc5448c9c7946a51f036a79234dc687a29
Ashish Bhungani <bhunganiashish@gmail.com>
2023-10-17 20:55:53 +0530

    A

--- dist/app.js
+++ dist/app.js
@@ -9,8 +9,14 @@ const middleware_1 = require("./middleware");
 const mongoose_1 = require("./config/mongoose");
 const routes_1 = require("./config/routes");
 const secrets_1 = require("./config/secrets");
+// import cros from 'cors'
 (0, mongoose_1.setupMongoose)(secrets_1.MONGO_URI);
 const app = (0, express_1.default)();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 (0, express_2.setupExpress)(app);
 (0, routes_1.setupRoutesV1)(app);
 // TODO: configure CORS
