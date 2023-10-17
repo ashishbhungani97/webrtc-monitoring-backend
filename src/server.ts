28bc54dee824ced2707cfc628b9688e9e249f6f8
Ashish Bhungani <bhunganiashish@gmail.com>
2023-10-17 20:43:29 +0530

    A

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,16 @@ import { createServer } from 'http';
 import app from './app';
 import logger from './util/logger';
 import configSockets from './config/sockets';
+const cors = require('cors');
+
 
 const httpServer = createServer(app);
 configSockets(httpServer);
+
+
+app.use(cors({
+  origin: '*'
+}));
 
 httpServer.listen(app.get('port'), (): void => {
   logger.info(
