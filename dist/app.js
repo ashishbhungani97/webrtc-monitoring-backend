40243943c005b0efd5de0431e721919fd7bcb341
Ashish Bhungani <bhunganiashish@gmail.com>
2023-10-17 21:20:21 +0530

    A

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,20 @@ const secrets_1 = require("./config/secrets");
 const cors_1 = __importDefault(require("cors"));
 (0, mongoose_1.setupMongoose)(secrets_1.MONGO_URI);
 const app = (0, express_1.default)();
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://reliable-tapioca-f7eba3.netlify.app');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 (0, express_2.setupExpress)(app);
 (0, routes_1.setupRoutesV1)(app);
 // TODO: configure CORS
--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,26 @@ setupMongoose(MONGO_URI);
 
 const app = express();
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://reliable-tapioca-f7eba3.netlify.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 setupExpress(app);
 setupRoutesV1(app);
